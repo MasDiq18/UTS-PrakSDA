ce7ad67a4bb3c56e1dfb2c544bf2a6f496251a64
Muhammad Farhan Alafif <farhanfifel@gmail.com>
2025-03-26 16:10:53 +0700

    add function
    add function postfixtoinfix and main

--- parsing_expression.c
+++ parsing_expression.c
@@ -22,7 +22,7 @@ int isEmpty(Stack *s) {
 }
 
 // Push ke stack
-void push(Stack *s, char *str) {
+void push(Stack *s, const char *str) {
     if (s->top < MAX - 1) {
         s->top++;
         strcpy(s->arr[s->top], str);
@@ -63,30 +63,114 @@ int isOperator(char ch) {
 }
 
 // Fungsi konversi dari Infix ke Postfix
-void infixToPostfix(char *infix, char *postfix) {
+void infixToPostfix(const char *infix, char *postfix) {
     Stack operators;
     init(&operators);
     int i = 0, k = 0;
-    char token, temp[MAX];
+    char token;
+    char buffer[MAX];
+
     while ((token = infix[i++]) != '\0') {
         if (isalnum(token)) {
-            postfix[k++] = token; // Operand langsung dimasukkan ke hasil
+            int j = 0;
+            while (isalnum(token)) {
+                buffer[j++] = token;
+                token = infix[i++];
+            }
+            buffer[j] = '\0';
+            i--;
+            sprintf(postfix + k, "%s ", buffer);
+            k += strlen(buffer) + 1;
         } else if (token == '(') {
-            push(&operators, (char[]){token, '\0'});
+            push(&operators, "(");
         } else if (token == ')') {
-            while (!isEmpty(&operators) && (peek(&operators))[0] != '(') {
-                postfix[k++] = pop(&operators)[0];
+            while (!isEmpty(&operators) && strcmp(peek(&operators), "(") != 0) {
+                sprintf(postfix + k, "%s ", pop(&operators));
+                k += 2;
             }
             pop(&operators);
         } else if (isOperator(token)) {
             while (!isEmpty(&operators) && precedence(peek(&operators)[0]) >= precedence(token)) {
-                postfix[k++] = pop(&operators)[0];
+                sprintf(postfix + k, "%s ", pop(&operators));
+                k += 2;
             }
-            push(&operators, (char[]){token, '\0'});
+            char opStr[2] = {token, '\0'};
+            push(&operators, opStr);
         }
     }
     while (!isEmpty(&operators)) {
-        postfix[k++] = pop(&operators)[0];
+        sprintf(postfix + k, "%s ", pop(&operators));
+        k += 2;
     }
-    postfix[k] = '\0';
+    postfix[k - 1] = '\0';
+}
+
+// Fungsi konversi dari Postfix ke Infix
+void postfixToInfix(const char *postfix, char *infix) {
+    Stack operands;
+    init(&operands);
+    int i = 0;
+    char token;
+    char buffer[MAX];
+
+    while ((token = postfix[i++]) != '\0') {
+        if (isalnum(token)) {
+            int j = 0;
+            while (isalnum(token)) {
+                buffer[j++] = token;
+                token = postfix[i++];
+            }
+            buffer[j] = '\0';
+            i--;
+            push(&operands, buffer);
+        } else if (isOperator(token)) {
+            if (isEmpty(&operands)) {
+                printf("Ekspresi postfix tidak valid.\n");
+                return;
+            }
+            char op2[MAX], op1[MAX], expr[MAX];
+            strcpy(op2, pop(&operands));
+            strcpy(op1, pop(&operands));
+
+            sprintf(expr, "(%s %c %s)", op1, token, op2);
+            push(&operands, expr);
+        }
+    }
+
+    if (!isEmpty(&operands)) {
+        strcpy(infix, pop(&operands));
+    }
+}
+
+// **MAIN FUNCTION untuk memilih fungsi yang ingin dijalankan**
+int main() {
+    char input[MAX], output[MAX] = "";
+    int pilihan;
+
+    printf("Pilih Konversi:\n");
+    printf("1. Infix ke Postfix\n");
+    printf("2. Postfix ke Infix\n");
+    printf("Masukkan pilihan (1/2): ");
+    scanf("%d", &pilihan);
+    getchar(); // Menghapus newline dari buffer
+
+    if (pilihan == 1) {
+        printf("Masukkan ekspresi infix: ");
+        fgets(input, MAX, stdin);
+        input[strcspn(input, "\n")] = 0;
+
+        infixToPostfix(input, output);
+        printf("Ekspresi dalam Postfix: %s\n", output);
+    } else if (pilihan == 2) {
+        printf("Masukkan ekspresi postfix: ");
+        fgets(input, MAX, stdin);
+        input[strcspn(input, "\n")] = 0;
+
+        postfixToInfix(input, output);
+        printf("Ekspresi dalam Infix: %s\n", output);
+    } else {
+        printf("Pilihan tidak valid.\n");
+    }
+
+    return 0;
 }
